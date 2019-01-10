3f2f4a6a992a4fab026d041bb2bfc2caba464fb0
kajiecy <851416347@qq.com>
2019-01-10 18:13:13 +0800

    编写 正在 模块

--- js/urlInfo.js
+++ js/urlInfo.js
@@ -2,5 +2,6 @@ const baseUrl = "http://localhost:8080/mvc";
 let urlInfo = {
     login: baseUrl + '/common/login.json',
     getNewsRecommend: baseUrl + '/news/getNewsRecommend.json',
-
+    getNewsOutList4App: baseUrl + '/news/getNewsOutList4App.json',
+    getNewsInnerList4App: baseUrl + '/news/getNewsInnerList4App.json',
 }
