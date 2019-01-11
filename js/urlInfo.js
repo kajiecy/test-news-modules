49a6049b5e83ea4032f44918b1fbefaa7290aea9
kajiecy <851416347@qq.com>
2019-01-11 16:04:39 +0800

    美股

--- js/urlInfo.js
+++ js/urlInfo.js
@@ -1,7 +1,8 @@
-const baseUrl = "http://localhost:8080/mvc";
+const baseUrl = "http://apps.forexspace.cn/mtinter/mvc";
 let urlInfo = {
     login: baseUrl + '/common/login.json',
     getNewsRecommend: baseUrl + '/news/getNewsRecommend.json',
     getNewsOutList4App: baseUrl + '/news/getNewsOutList4App.json',
     getNewsInnerList4App: baseUrl + '/news/getNewsInnerList4App.json',
+    addPraise: baseUrl + '/news/addPraise.json',
 }
